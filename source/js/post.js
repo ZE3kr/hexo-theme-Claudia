189a95ac23e070028d59a35092f8ad136e46b179
ZE3kr <ze3kr@icloud.com>
2021-12-02 03:24:02 +0800

    Update post.js

--- source/js/post.js
+++ source/js/post.js
@@ -1,25 +1,27 @@
 var $posts = {
-    scroller: function () {
+    scroller: function() {
         function Scroller() {
             this.callbacks = []
             return this
         }
-        Scroller.prototype.bindScrollEvent = function () {
+        Scroller.prototype.bindScrollEvent = function() {
             var _that = this
 
-            window.addEventListener('scroll', function (event) {
+            window.addEventListener('scroll', function(event) {
                 var wait = false
                 var beforeOffsetY = window.pageYOffset
 
                 if (wait) return
                 wait = true
 
-                setTimeout(function () {
+                setTimeout(function() {
                     var params = {
                         event: event,
                         beforeOffsetY: beforeOffsetY,
                     }
-                    _that.callbacks.forEach(function (func) { func(params) })
+                    _that.callbacks.forEach(function(func) {
+                        func(params)
+                    })
 
                     wait = false
                 }, 150)
@@ -28,7 +30,7 @@ var $posts = {
 
         return Scroller
     },
-    showTopic: function (evt) {
+    showTopic: function(evt) {
         var topicEl = document.getElementById('postTopic')
         var postTitle = document.getElementById('postTitle')
 
@@ -50,8 +52,7 @@ var $posts = {
                 if (topicEl.classList.contains('is-show-scrollToTop-tips')) {
                     topicEl.classList.remove('is-show-scrollToTop-tips')
                     topicEl.classList.add('is-flash-scrollToTop-tips')
-                }
-                else {
+                } else {
                     topicEl.classList.add('immediately-show')
                 }
             }
@@ -84,8 +85,7 @@ var $posts = {
                 topicEl.classList.remove('is-flash-scrollToTop-tips')
                 topicEl.classList.add('is-show-post-title')
             }
-        }
-        else{
+        } else {
             // hidden all
             topicEl.classList.remove('is-flash-scrollToTop-tips')
             topicEl.classList.remove('is-show-scrollToTop-tips')
@@ -96,18 +96,18 @@ var $posts = {
             topicEl.classList.add('is-hidden-topic-bar')
         }
     },
-    catalogueHighlight: function () {
+    catalogueHighlight: function() {
         var directory = document.querySelectorAll('.toc a')
         if (directory.length === 0) {
             return false
         }
 
         var tocContainer = document.querySelector('.toc')
-        return function () {
+        return function() {
             var contentTocList = []
             var activeClassName = 'is-active'
 
-            directory.forEach(function (link) {
+            directory.forEach(function(link) {
                 if (!link.href) return
                 var id = decodeURI(link.href).split('#')[1]
                 contentTocList.push(document.getElementById(id))
@@ -142,9 +142,10 @@ var $posts = {
                         // top: direc.offsetTop - spacing,
                         top: direc.offsetTop + 100 - tocContainerHeight,
                     })
-                }
-                else {
-                    tocContainer.scrollTo({ top: 0 })
+                } else {
+                    tocContainer.scrollTo({
+                        top: 0
+                    })
                 }
             }
         }
@@ -166,7 +167,7 @@ var $posts = {
             appKey: el.dataset.comment_valine_key
         })
     },
-    mounted: function () {
+    mounted: function() {
         typeof hljs !== 'undefined' && hljs.initHighlighting()
 
         var Scroller = this.scroller()
@@ -184,6 +185,125 @@ var $posts = {
         document.getElementById('postTopic').addEventListener('click', this.smoothScrollToTop)
 
         window.Valine && this.addValineComment()
+
+        var timeout
+
+        function postScroll() {
+            if (timeout) {
+                clearTimeout(timeout)
+            }
+            timeout = setTimeout(function() {
+                document.querySelectorAll('.content figure').forEach(function(figure) {
+                    if (figure.getBoundingClientRect().top + figure.clientHeight < window.innerHeight && figure.getBoundingClientRect().top > 58) {
+                        figure.classList.add('hover')
+                        hasHover = true
+                    } else {
+                        figure.classList.remove('hover')
+                    }
+                })
+            }, 100)
+        }
+
+        var canScroll = true
+        window.addEventListener('load', function() {
+            setTimeout(postScroll, 500)
+        })
+        window.addEventListener('scroll', postScroll)
+        document.querySelectorAll('.content figure').forEach(function(figure) {
+            window.addEventListener('load', function() {
+                figure.querySelectorAll('iframe').forEach(function(iframe) {
+                    var url = new URL(iframe.src)
+                    if (url.searchParams.get('autoplay') !== 'true') {
+                        var player = Stream(iframe)
+                        player.addEventListener('play', () => {
+                            figure.classList.add('full-screen')
+                            window.$claudia.disableScroll()
+                        })
+                        player.addEventListener('pause', () => {
+                            figure.classList.remove('full-screen')
+                            window.$claudia.enableScroll()
+                        })
+                    }
+                })
+            })
+
+            var img = figure.querySelector('img')
+
+            img.scale = 100
+            img.translateX = 0
+            img.translateY = 0
+            img.scaleBase = 100
+
+            figure.addEventListener('touchstart', function(e) {
+                img.scaleBase = img.scale
+            })
+            figure.addEventListener('touchmove', function(e) {
+                if (e.scale !== 1) {
+                    e.preventDefault()
+                    img.scale = e.scale * img.scaleBase
+                    if (img.scale < 100) {
+                        img.scale = 100
+                    }
+                    var transform = `translateY(-50%) scale(${img.scale / 100})`
+                    if (transform !== img.style.transform) {
+                        img.style.transform = transform
+                    }
+                }
+            })
+
+            figure.addEventListener(window.$claudia.wheelEvent, function(e) {
+                e.stopPropagation()
+                if (figure.classList.contains('full-screen')) {
+                    if (e.ctrlKey && e.deltaY !== 0) {
+                        img.scale -= e.deltaY
+                        figure.scrollLeft -= e.deltaY * window.innerWidth / 200
+                        figure.scrollTop -= e.deltaY * window.innerHeight / 200
+                        img.translateY = figure.scrollTop * 100 / window.innerHeight
+                        if (img.scale < 100) {
+                            img.scale = 100
+                        }
+                        var transform = `translateY(-50%) scale(${img.scale / 100})`
+                        if (transform !== img.style.transform) {
+                            img.style.transform = transform
+                        }
+                        return
+                    }
+
+                    img.translateY += e.deltaY * 100 / window.innerHeight
+                    if (img.translateY > img.scale) {
+                        img.translateY = img.scale
+                    }
+                    if (img.translateY < 0) {
+                        img.translateY = 0
+                    }
+                    figure.scrollTop = img.translateY / 100 * window.innerHeight
+                }
+            })
+
+            figure.addEventListener('click', function() {
+                if (figure.classList.contains('full-screen')) {
+                    figure.classList.remove('full-screen')
+                    img.style.transform = ''
+                    canScroll = true
+                    window.$claudia.enableScroll()
+                } else {
+                    img.scale = 100
+                    img.translateX = 0
+                    img.translateY = 0
+                    figure.querySelectorAll('img').forEach(function(img) {
+                        if (img.offsetWidth >= 2 * img.offsetHeight) {
+                            figure.classList.add('panorama')
+                            img.sizes = (window.innerHeight / img.offsetHeight * img.offsetWidth) + 'px'
+                        } else {
+                            img.sizes = 'max(100vw, 100vh)'
+                        }
+                    })
+                    figure.classList.add('full-screen')
+                    canScroll = false
+                    window.$claudia.disableScroll()
+                }
+            })
+        })
     }
 }
 
